f5e4db77b9e6c022bb076000a13d2eb16f6e448d
sh4869 <nobuhiro0917k@ybb.ne.jp>
2014-05-30 08:27:43 +0900

    first commit - add ame_yame.rb and keys.rb

---
+++ ame_yame.rb
@@ -0,0 +1,18 @@
+# Coding: UTF-8
+require 'twitter'
+require 'keys.rb'
+
+@rest_client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = CONSUMER_KEY
+  config.consumer_secret     = CONSUMER_SECRET
+  config.access_token        = ACCESS_TOKEN
+  config.access_token_secret = ACCESS_SECRET
+end
+
+@stream_client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key       = CONSUMER_KEY
+  config.consumer_secret    = CONSUMER_SECRET
+  config.access_token        = ACCESS_TOKEN
+  config.access_token_secret = ACCESS_SECRET
+end
+
