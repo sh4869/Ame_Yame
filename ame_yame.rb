c6270016ee096df4c2035d372380114864fcb780
sh4869 <nobuhiro0917k@ybb.ne.jp>
2014-05-30 22:59:27 +0900

    update ame_yame.rb

--- ame_yame.rb
+++ ame_yame.rb
@@ -1,6 +1,10 @@
 # Coding: UTF-8
 require 'twitter'
-require 'keys.rb'
+require './keys.rb'
+require 'MeCab'
+
+mecab = MeCab::Tagger.new
+@f_1 = 0
 
 @rest_client = Twitter::REST::Client.new do |config|
   config.consumer_key        = CONSUMER_KEY
@@ -14,5 +18,38 @@ end
   config.consumer_secret    = CONSUMER_SECRET
   config.access_token        = ACCESS_TOKEN
   config.access_token_secret = ACCESS_SECRET
+end
+
+def check
+  if @f_1 == 1
+	sleep(60)
+	@f_1 = 0
+  end
+end
+
+loop do
+  @stream_client.user do |object|
+	if object.is_a?(Twitter::Tweet) 
+	  if object.user.screen_name != "sh4869bot"
+		if object.uris? == false && object.media? == false && object.user_mentions? == false
+		  sentence = object.text
+		  node = mecab.parseToNode(sentence)
+		  word_array = []
+		  begin
+			node = node.next
+			if /^名詞/ =~ node.feature.force_encoding("UTF-8")
+			  word_array << node.surface.force_encoding("UTF-8")
+			end
+		  end until node.next.feature.include?("BOS/EOS")
+		  word = word_array.sample
+		  if word != nil
+			@rest_client.update(word + "やめー!")
+			@f_1 = 1
+		  end
+		end
+	  end
+	end
+  check
+  end
 end
 
--- mecab_test.rb
+++ mecab_test.rb
@@ -1,4 +1,19 @@
 #encoding: utf-8
+# 
 require 'MeCab'
-c = MeCab::Tagger.new
-puts c.parse("すもももももももものうち")
+puts "解析したい文章を入力してください。"
+
+sentence = gets.chomp
+#  
+mecab = MeCab::Tagger.new
+node = mecab.parseToNode(sentence)
+word_array = []
+#   
+begin
+  node = node.next
+  if /^名詞/ =~ node.feature.force_encoding("UTF-8")
+	  word_array << node.surface.force_encoding("UTF-8")
+  end
+end until node.next.feature.include?("BOS/EOS")
+
+puts word_array
