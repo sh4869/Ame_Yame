f34b652a62d8460e5163063acf02bdf942be419e
sh4869 <nobuhiro0917k@ybb.ne.jp>
2014-05-31 14:40:10 +0900

    change favorite time and puts word

--- ame_yame.rb
+++ ame_yame.rb
@@ -25,7 +25,6 @@ loop do
 	if object.is_a?(Twitter::Tweet) 
 	  if object.user.screen_name != "sh4869bot"
 		if object.uris? == false && object.media? == false && object.user_mentions? == false
-          @rest_client.favorite(object.id)
 		  sentence = object.text
 		  node = mecab.parseToNode(sentence)
 		  word_array = []
@@ -37,6 +36,8 @@ loop do
 		  end until node.next.feature.include?("BOS/EOS")
 		  word = word_array.sample
 		  if word != nil
+			puts word
+			@rest_client.favorite(object.id)
 			@rest_client.update(word + "やめー!")
 			@f_1 = 1
 		  end
