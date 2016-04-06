0fe6f5e7ac0a782be6494da37044aae18450e40b
sh4869 <nobuk4869@gmail.com>
2016-04-06 22:19:02 +0900

    Update ame_yame.cr

--- src/ame_yame.cr
+++ src/ame_yame.cr
@@ -1,5 +1,4 @@
 require "./ame_yame/*"
 
-module AmeYame
-  # TODO Put your code here
-end
+word_extracter = Ame_Yame::WordExtracter.new("dj0zaiZpPUt3ZWM1OWFZNGg0eCZzPWNvbnN1bWVyc2VjcmV0Jng9NDk-")
+puts word_extracter.extract("雨やめbotで日本語を解析するのにつかいます。","名詞")
